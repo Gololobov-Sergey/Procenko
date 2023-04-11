af71f551bd7164a256f3834169053f7235cc0854
Gololobov-Serhiy <gololobov.serhiy@gmail.com>
2023-04-11 10:20:02 +0300

    11.04.2023

--- Procenko/Procenko.cpp
+++ Procenko/Procenko.cpp
@@ -2,14 +2,15 @@
 //
 
 #include <iostream>
-//parne ne parne 
+/*известно кол-во учеников в каждной из n школой города. 
+Определить кол-во школ в которой учится более 1к учеников.
+*/
 using namespace std;
 
 /*bool Foo(int x)
 {
 	return x % 2 == 0;
 } */
-
 
 //int NOD(int a, int b)
 //{
@@ -46,8 +47,88 @@ int main()
 	cout << Foo(x);
 }*/
 {
+	setlocale(LC_ALL, "ru");
+	
+	//int P = 1;
+
+	//for (int i = -3; i <= 3; i++)
+	//{
+	//	if (i != 0)
+	//	{
+	//		P *= i * i;
+	//	}
+	//}
+	//cout << P << endl;
+
+	//int n, S = 0;
+	//cin >> n;
+
+	//for (int i = 1;i <= n; i++)
+	//{
+	//cout << "кол-во магазинов: " << i << " :Сколько? ";
+	//int h;
+	//cin >> h;
+
+	//if (h > 10)
+	//{
+	//	S++;
+	//}
+	//cout << "кол-во магазинов которые могут продать: " << S << endl;
+
+
+
+
+	//int n, S = 0;
+	//cin >> n;
+	//
+	//for (int i = 1;i <= n; i++)
+	//{
+	//	cout << "кол-во учеников: " << i << " :Сколько?";
+	//	int h;
+	//	cin >> h;
+
+	//	if (h > 1000)
+	//	{
+	//		S++;
+	//	}
+	//}
+	//cout << "кол-во школ: " << S << endl;
+	//double N = 0.43;
+
+	//for (double i = 1; i <= 2; i+=0.2)
+	//{
+	//cout << i << " KG: " << i * N << "$" << endl;
+	//}
+
+
+
+
+	//double N = 0.43;
+
+	//for (double i = 0.1; i <= 1; i+=0.1)
+	//{
+	//	cout << i << " KG: " << i * N << "$" << endl;
+	//}
+
+
+	//int A = 3;
+	//int B = 6;
+
+	//for (int i = A; i <= B; i++)
+	//{
+	//	cout << i << endl;
+	//}
+	//cout << B - A + 1;
+	
 	//int a = 18, b = 12;
 	//cout << a << "/" << b << endl;
+	//int N = 5;
+	//int K = 3;
+
+	//for (int i = 0; i < N; i++)
+	//{
+	//	cout << K << endl;
+	//}
 
 	//int nod = NOD(a, b);
 
